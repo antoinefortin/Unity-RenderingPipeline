5db9ea98d00ba9a70d8ea61fbe4de4dc2bdffb52
unknown <fortin.antoine@cdrin.com>
2021-04-24 01:06:24 -0400

    Basic files pipeline

---
+++ CustomRP/Runtime/CameraRenderer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer 
+{
+
+    ScriptableRenderContext context;
+    Camera camera;
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+        DrawVisibleGeometry();
+        Submit();
+    }
+
+    void DrawVisibleGeometry()
+    {
+        context.DrawSkybox(camera);
+    }
+
+    void Submit()
+    {
+        context.Submit();
+    }
+}
